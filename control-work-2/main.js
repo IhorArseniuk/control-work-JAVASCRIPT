ceb0ecafd7bb51c1f4349c17d18badc714e64832
IhorArseniuk <ihorarsenykk.a@gmail.com>
2025-07-21 22:43:54 +0200

    work on cw2

--- control-work-2/main.js
+++ control-work-2/main.js
@@ -1,3 +1,19 @@
 fetch('https://jsonplaceholder.typicode.com/users')
 .then(response => response.json())
-.then(json => {})
+.then(users => {
+
+    for(let user of users) {
+
+        let userDiv=document.createElement('div');
+        userDiv.className='user-div';
+        userDiv.innerHTML=`<h2>${user.id}</h2>  <p>${user.name}</p> <p>${user.username}</p>`;
+
+        let buttonDetails=document.createElement('button');
+        buttonDetails.innerHTML=`<a href="html/user-details.html">Full info</a>`;
+
+        userDiv.appendChild(buttonDetails);
+
+        let usersDiv=document.getElementsByClassName('div-users')[0];
+        usersDiv.appendChild(userDiv);
+    }
+})
