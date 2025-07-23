d662e3976a1b24c092d388645e63776a866b4da6
IhorArseniuk <ihorarsenykk.a@gmail.com>
2025-07-23 22:28:21 +0200

    work on buttons on cw2

--- control-work-2/user-details-page/main.js
+++ control-work-2/user-details-page/main.js
@@ -47,6 +47,7 @@ userPhoneH3.innerText=`Phone:${user.phone}`;
 
 let userWebSiteA=document.createElement('a');
 userWebSiteA.href=`${user.website}`;
+userWebSiteA.innerText=`User Website:${user.website}`;
 
 let userCompany=user.company;
 let userCompanyDiv=document.createElement('div');
@@ -57,9 +58,53 @@ for(let item in userCompany){
     itemP.innerText=`${item}:${userCompany[item]}`;
   userCompanyDiv.appendChild(itemP);
 }
+let buttonsDiv=document.createElement('div');
+buttonsDiv.className='buttons-div';
 
 let postBtn=document.createElement('button');
 postBtn.innerText='post of current user'
+buttonsDiv.appendChild(postBtn);
 
-userDiv.append(userIdH2,userNameP,userUsrNamP,userEmailP, addressDiv,userPhoneH3,userWebSiteA,userCompanyDiv,postBtn);
+userDiv.append(userIdH2,userNameP,userUsrNamP,userEmailP, addressDiv,userPhoneH3,userWebSiteA,userCompanyDiv,buttonsDiv);
 userSection.appendChild(userDiv);
+
+postBtn.addEventListener('click',(ev)=>{
+
+    let currentUserPostsDiv=document.createElement('div');
+    currentUserPostsDiv.className='user-posts-div';
+    let postsH2=document.createElement('h2');
+    postsH2.innerText=' User posts:'
+    userSection.appendChild(postsH2);
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res=>res.json())
+        .then(posts=>{
+            for(let post of posts){
+                if(user.id===post.userId){
+                    let postDiv=document.createElement('div');
+                    postDiv.className='user-post-div';
+
+                    let postTitleH3=document.createElement('h3');
+                    postTitleH3.innerText=`Title:${post.title}`;
+                    let postDetailsBtn=document.createElement('button');
+                    postDetailsBtn.innerHTML=`<a href="../post-details-page/post-details.html">Post Details</a>`;
+                    postDiv.append(postTitleH3,postDetailsBtn);
+                    currentUserPostsDiv.appendChild(postDiv);
+                }
+            }
+            userSection.appendChild(currentUserPostsDiv);
+
+        })
+
+    let hidePostsBtn=document.createElement('button');
+            hidePostsBtn.innerText='Hide Posts';
+            buttonsDiv.appendChild(hidePostsBtn);
+
+            hidePostsBtn.addEventListener('click',()=>{
+                postsH2.remove() ;
+                currentUserPostsDiv.remove();
+            })
+
+
+        postBtn.disabled=currentUserPostsDiv.hasChildNodes(true);
+})
